aea360d8ee4b580b7cacd13db98e6e52f3611125
Robert Clarke <r.jason.clarke@gmail.com>
2020-12-15 00:08:02 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,8 +1,18 @@
 class Dog
-  def initialize name=(rudolph)
-    @name = "Rudolph"
+
+  attr_accessor :name, :breed, :age
+
+    @@all = []
+
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
   end
-  def name
-    @name
+
+  def self.all
+      @@all
   end
+
 end
